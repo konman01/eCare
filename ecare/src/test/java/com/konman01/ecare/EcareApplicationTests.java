f38a5afbc9ac7072ac4c95127489e0b07ef280da
konman01 <konimanjunatha@gmail.com>
2022-07-08 19:00:51 -0500

    Initial Check in of the Project

---
+++ ecare/src/test/java/com/konman01/ecare/EcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konman01.ecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
